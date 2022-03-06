a458a0599f8d87c80fc52dfcfa942a24771da3b3
Magdalena.Morag <Magdalena.Morag@PK5-16>
2022-03-06 12:31:19 +0100

    Creating the StandardGradeBook class

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -6,5 +6,9 @@ namespace GradeBook.GradeBooks
 {
     class StandardGradeBook : BaseGradeBook
     {
+        public StandardGradeBook(string name):base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
     }
 }
