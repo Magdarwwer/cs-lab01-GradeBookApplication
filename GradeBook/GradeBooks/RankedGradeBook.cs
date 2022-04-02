c1065ba781cf6ef0b3423d95aa7cfbedc7c8acc1
Magda <Magda@LAPTOP-K7D4V89L>
2022-04-02 16:41:43 +0200

    Update HelpCommand,Update BaseGradeBook.GetGPA, Refactor constructor of BaseGradeBook - FINISHED

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook: BaseGradeBook
+    public class RankedGradeBook: BaseGradeBook
     {
         public RankedGradeBook(string name, bool param) : base(name, param)
         {
@@ -19,94 +19,38 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Less than 5 students");
             }
 
-
-            int fifth = Students.Count / 5;
-            int countGreater = 0;
-            for (int i = 0; i < Students.Count-1; i++)
+            int onefifth = Students.Count / 5;
+            int countgreater = 0;
+            for (int i = 0; i < Students.Count; i++)
             {
-                
-                if(averageGrade <= Students[i].AverageGrade)
-                {
-                    countGreater++;
-                }
-                
 
-                
-                //if (averageGrade <= Students[i].AverageGrade)
-                //{
-                //    int temp = 
-                //}
+                if (averageGrade <= Students[i].AverageGrade)
+                {
+                    countgreater++;
+                }
+
             }
 
-            if (countGreater <= fifth)
+            if (countgreater <= onefifth)
             {
                 return 'A';
-            } 
-            else if (countGreater <= 2 * fifth)
+            }
+            else if (countgreater > onefifth && countgreater <= 2 * onefifth)
             {
                 return 'B';
             }
-            else if (countGreater <= 3 * fifth)
+            else if (countgreater > 2 * onefifth && countgreater <= 3 * onefifth)
             {
                 return 'C';
-            }              
-            else if (countGreater <= 4 * fifth)
-            {
-                return 'D';
-            }   
-            else
-            {
-                return 'F';
             }
-
-            /*
-            //sortowanie
-            var result = Students.OrderBy(item => item.AverageGrade);
-           // var result2 = from item in Students orderby item.AverageGrade select item;
-            Students = result.ToList();
-            //List<Student> list = result2.ToList();
-
-            int progpkt = Students.Count / 5;
-
-            if (averageGrade <= Students.Count / 5)
-            {
-                return 'F';
-            }
-            else if (averageGrade > Students.Count / 5 && averageGrade <= 2 * (Students.Count / 5))
+            else if (countgreater > 3 * onefifth && countgreater <= 4 * onefifth)
             {
                 return 'D';
             }
             else
-                return 'A';
-
-            */
-
-            //if (averageGrade <= Students[progpkt].AverageGrade)
-            //{
-            //    return 'F';
-            //}
-            //else if (averageGrade > Students[progpkt].AverageGrade && averageGrade <= Students[2 * progpkt].AverageGrade)
-            //{
-            //    return 'D';
-            //}
-            //else if (averageGrade > Students[2 * progpkt].AverageGrade && averageGrade <= Students[3 * progpkt].AverageGrade)
-            //{
-            //    return 'C';
-            //}
-            //else if (averageGrade > Students[3 * progpkt].AverageGrade && averageGrade <= Students[4 * progpkt].AverageGrade)
-            //{
-            //    return 'B';
-            //} 
-            //else //if (averageGrade > Students[4 * progpkt].AverageGrade && averageGrade <= Students[5 * progpkt].AverageGrade)
-            //{
-            //    return 'A';
-            //}
-
-            //int lastindex = students.count - 1;
-            //for(int i = lastindex; i > lastindex - progpkt; i--)
-            //{
-
-            //}
+            {
+                return 'F';
+            }
         }
 
         public override void CalculateStatistics()
